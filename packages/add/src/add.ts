0dd8593e041d879f99dfaf62519a46faea84a8ae
mdh <mdh@bivens.dev>
2022-09-25 21:03:11 +0100

    Remove example packages

--- packages/add/src/add.ts
+++
@@ -1,8 +0,0 @@
-import { Operation } from "@usrbinboat/core";
-
-export class Add extends Operation {
-    sym = "+";
-    protected exec(a: number, b: number): number {
-        return a + b;
-    }
-}
