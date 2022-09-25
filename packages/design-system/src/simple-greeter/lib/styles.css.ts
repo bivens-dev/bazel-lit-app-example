c272de92fd42f427efee2ffbf45ee90b137282ab
mdh <mdh@bivens.dev>
2022-09-25 20:30:36 +0100

    Fix up some outstanding things

---
+++ packages/design-system/src/simple-greeter/lib/styles.css.ts
@@ -0,0 +1,3 @@
+import { css } from 'lit';
+
+export const defaultStyle = css`p { color: blue }`;
--- packages/design-system/src/simple-greeter/simple-greeter.ts
+++ packages/design-system/src/simple-greeter/simple-greeter.ts
@@ -1,9 +1,10 @@
-import { css, html, LitElement } from 'lit';
+import { html, LitElement } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
+import { defaultStyle } from './lib/styles.css.js';
 
 @customElement('simple-greeting')
 export class SimpleGreeting extends LitElement {
-  static override styles = css`p { color: blue }`;
+  static override styles = [defaultStyle];
 
   @property()
   name = 'Somebody';
